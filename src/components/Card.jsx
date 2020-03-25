4a941d9656cdc221b5ef17700e2a37f228e2f54a
Aaron Smith <aaroncosmith@gmail.com>
2020-03-25 15:50:06 -0400

    getting data from api, assigning that data to our states, then updating them, then putting them on the page, added multipul cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,8 +15,17 @@ class Card extends Component {
             const response = await fetch('https://randomuser.me/api/?results=1')
             const data = await response.json();
             console.log(data.results[0]);
+            console.log(data.results[0].location.street.number);
             this.setState({
-                photo: data.results[0].picture.large
+                photo: data.results[0].picture.large,
+                name: data.results[0].name.first + " " + data.results[0].name.last,
+                email: data.results[0].email,
+                birthday: data.results[0].dob.age,
+                address: data.results[0].location.street.number + " " + data.results[0].location.street.name + ", " + data.results[0].location.city + ", " + data.results[0].location.state + ", " + data.results[0].location.postcode +" " + data.results[0].location.country,
+                phone: data.results[0].cell,
+                password: data.results[0].login.password
+
+
             })
         } catch (error) {
             console.log("error is ", error.message)
@@ -31,8 +40,8 @@ class Card extends Component {
                 </div>
                 <div className="information-holder">
                     <p>Hello, my name is {name}!</p>
+                    <p>I am {birthday} years old!</p>
                     <p>My email is {email}.</p>
-                    <p>{birthday} is my birthday!</p>
                     <p>You can find me at {address}.</p>
                     <p>Shoot me a ring at {phone}</p>
                     <p>{password} is totally not my password.</p>
